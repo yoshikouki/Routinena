9bd9f77524027374e1a9e7576a3b6352215e92d1
yoshikouki <yoshikouki@gmail.com>
2023-07-02 14:23:22 +0900

    Show Auth status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-// import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
+import { auth } from "~/server/auth";
 // import { api } from "~/utils/api";
 
 export default function Home() {
@@ -47,7 +47,7 @@ export default function Home() {
               {/* {hello.data ? hello.data.greeting : "Loading tRPC query..."} */}
               {"Loading tRPC query..."}
             </p>
-            {/* <AuthShowcase /> */}
+            <AuthShowcase />
           </div>
         </div>
       </main>
@@ -55,26 +55,35 @@ export default function Home() {
   );
 }
 
-// function AuthShowcase() {
-//   const { data: sessionData } = useSession();
+async function AuthShowcase() {
+  const session = await auth();
 
-//   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-//     undefined, // no input
-//     { enabled: sessionData?.user !== undefined }
-//   );
+  // const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+  //   undefined, // no input
+  //   { enabled: sessionData?.user !== undefined }
+  // );
 
-//   return (
-//     <div className="flex flex-col items-center justify-center gap-4">
-//       <p className="text-center text-2xl text-white">
-//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-//         {secretMessage && <span> - {secretMessage}</span>}
-//       </p>
-//       <button
-//         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-//         onClick={sessionData ? () => void signOut() : () => void signIn()}
-//       >
-//         {sessionData ? "Sign out" : "Sign in"}
-//       </button>
-//     </div>
-//   );
-// }
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {session && <span>Logged in as {session.user.name}</span>}
+        {/* {secretMessage && <span> - {secretMessage}</span>} */}
+      </p>
+      {session ? (
+        <Link
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+          href={"/api/auth/signout"}
+        >
+          Sign out
+        </Link>
+      ) : (
+        <Link
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+          href={"/api/auth/signin"}
+        >
+          Sign in
+        </Link>
+      )}
+    </div>
+  );
+}
