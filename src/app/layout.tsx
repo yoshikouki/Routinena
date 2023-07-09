7045ce1091a9cccf917f79e2c789d77303abf4f3
yoshikouki <yoshikouki@gmail.com>
2023-07-09 11:32:08 +0900

    Update favicon and other icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,14 +7,25 @@ export const metadata: Metadata = {
   title: "ルーティンナさん | Routinena",
   description:
     "「ルーティンナさん」はあなたの定期的なタスクを追跡し、最後にいつ完了したのかを簡単に記録できるWebアプリです。あなたの忙しい日常をサポートし、タスク管理を効率的にします。",
-  icons: "/favicon.ico",
   viewport: {
     width: "device-width",
     initialScale: 1,
     maximumScale: 1,
   },
+
   // PWA config
   manifest: "/manifest.webmanifest",
+
+  // <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  // <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  // <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
+  },
 };
 
 export default function RootLayout({
