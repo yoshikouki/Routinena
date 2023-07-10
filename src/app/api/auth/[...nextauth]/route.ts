384e5e686dc1582ff3dae4190e116c3995c24a08
yoshikouki <yoshikouki@gmail.com>
2023-07-10 10:50:25 +0900

    Fix to build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const handlers = NextAuth(authOptions);
 export { handlers as GET, handlers as POST };
