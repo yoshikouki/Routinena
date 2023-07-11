7da3200e9ea84fb9620d6f747bedbcce38711358
yoshikouki <yoshikouki@gmail.com>
2023-07-11 23:54:40 +0900

    Update icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,10 +22,10 @@ export const metadata: Metadata = {
   // <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
-    apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
+    apple: { url: "/icons/apple-touch-icon.png", sizes: "180x180" },
   },
 };
 
