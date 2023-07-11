100a31529650bf7ade2c9b1f196c03657404cdcb
yoshikouki <yoshikouki@gmail.com>
2023-07-11 23:56:43 +0900

    Clean root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,11 @@
-import DashboardButton from "~/components/DashboardButton";
 import InstallationButton from "./InstallationButton";
-import LoginButton from "~/components/LoginButton";
-import LogoutButton from "~/components/LogoutButton";
-import { getServerAuthSession } from "~/server/auth";
 import LandingPageLayout from "~/components/layouts/LandingPageLayout";
 
-export default async function Home() {
-  const session = await getServerAuthSession();
-
+export default function Home() {
   return (
     <LandingPageLayout>
       <main className="flex min-h-screen min-w-full flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <div className="flex flex-col items-center gap-2">
-            <DashboardButton />
-          </div>
-          <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl">
-              {session && <span>Logged in as {session.user.name}</span>}
-            </p>
-            {session ? <LogoutButton /> : <LoginButton />}
-          </div>
-
-          <InstallationButton />
-        </div>
+        <InstallationButton />
       </main>
     </LandingPageLayout>
   );
