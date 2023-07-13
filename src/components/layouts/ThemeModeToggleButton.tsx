8c403bca699d745433ae7c29fcd7498ad95f0d87
yoshikouki <yoshikouki@gmail.com>
2023-07-13 22:08:59 +0900

    Enable mode toggle button

--- src/components/layouts/ThemeModeToggleButton.tsx
+++ src/components/layouts/ThemeModeToggleButton.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { DarkMode, LightMode } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
+import { IconButton, useColorScheme } from "@mui/material";
 
 const ThemeModeToggleButton = () => {
+  const { mode, setMode } = useColorScheme();
+  const modeString = mode === "light" ? "dark" : "light";
   return (
-    // <IconButton onClick={toggleThemeMode} aria-label={`${mode} mode`}>
-    //   {mode === "dark" ? <DarkMode /> : <LightMode />}
-    // </IconButton>
-    <IconButton>
-      <DarkMode />
-      <LightMode />
+    <IconButton
+      onClick={() => {
+        setMode(modeString);
+      }}
+      aria-label={`${modeString} mode`}
+    >
+      {mode === "light" ? <LightMode /> : <DarkMode />}
     </IconButton>
   );
 };
