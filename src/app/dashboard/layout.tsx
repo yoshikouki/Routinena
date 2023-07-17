5e81e19492ae54bccb8a7710c7199416448f0e42
yoshikouki <yoshikouki@gmail.com>
2023-07-17 11:57:33 +0900

    Enforce authentication

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,17 @@
 import { type Metadata } from "next";
+import { redirect } from "next/navigation";
+import { getServerAuthSession } from "~/server/auth";
 import AppLayout from "../../components/layouts/AppLayout";
 
 export const metadata: Metadata = {
   title: "ダッシュボード - ルーティンナさん | Routinena",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerAuthSession();
+  if (!session) {
+    redirect("/login");
+  }
+
   return <AppLayout>{children}</AppLayout>;
 }
