dc8c7c68c7a08244856004d156ceb51310556cbb
yoshikouki <yoshikouki@gmail.com>
2023-07-19 21:11:52 +0900

    I'll delete it once to differentiate 404 errors in production.

--- src/app/dashboard/activities/new/page.tsx
+++ src/app/dashboard/activities/new/page.tsx
@@ -1,16 +1,10 @@
 import NewActivity from "./NewActivity";
 import { type Metadata } from "next";
-import { redirect } from "next/navigation";
-import { getServerAuthSession } from "~/server/auth";
 
 export const metadata: Metadata = {
   title: "ダッシュボード - ルーティンナさん | Routinena",
 };
 
-export default async function NewActivityPage() {
-  const session = await getServerAuthSession();
-  if (!session) {
-    redirect("/login");
-  }
+export default function NewActivityPage() {
   return <NewActivity />;
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,10 @@
 import { type Metadata } from "next";
-import { redirect } from "next/navigation";
-import { getServerAuthSession } from "~/server/auth";
 import AppLayout from "../../components/layouts/AppLayout";
 
 export const metadata: Metadata = {
   title: "ダッシュボード - ルーティンナさん | Routinena",
 };
 
-export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const session = await getServerAuthSession();
-  if (!session) {
-    redirect("/login");
-  }
-
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return <AppLayout>{children}</AppLayout>;
 }
