7baeb174f5fb13a98f5d4dcbaaf59b6b757ff6dc
yoshikouki <yoshikouki@gmail.com>
2023-07-20 20:48:38 +0900

    Narrow the conditions even further

--- src/app/[activityId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { type Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "活動 - ルーティンナさん | Routinena",
-};
-
-export default function ActivityPage({ params }: { params: { activityId: string } }) {
-  return <>{params.activityId}</>;
-}
---
+++ src/app/[test]/page.tsx
@@ -0,0 +1,3 @@
+export default function ActivityPage({ params }: { params: { test: string } }) {
+  return <>{params.test}</>;
+}
