cdf0410b1d4b03a71562a5e936296b9526a780bd
yoshikouki <yoshikouki@gmail.com>
2023-07-20 21:09:05 +0900

    To isolate the cause for 404 error 3

--- next.config.mjs
+++ next.config.mjs
@@ -5,18 +5,6 @@
 await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
-const config = {
-
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-};
+const config = {};
 
 export default config;
