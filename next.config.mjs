e11a92611fd11c9b0ac7b8ccacdc8582d9b51693
yoshikouki <yoshikouki@gmail.com>
2023-07-20 22:14:53 +0900

    Revert i18n config to out

--- next.config.mjs
+++ next.config.mjs
@@ -14,10 +14,10 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en", "ja"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ["en", "ja"],
+  //   defaultLocale: "ja",
+  // },
 };
 
 export default config;
