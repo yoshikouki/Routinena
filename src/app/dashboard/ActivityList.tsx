3804be81d47833f5ceaa74ce71c736a51627ebbf
yoshikouki <yoshikouki@gmail.com>
2023-07-22 19:43:27 +0900

    Some MUI components can be rendered on the server

--- src/app/dashboard/ActivityList.tsx
+++ src/app/dashboard/ActivityList.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { Skeleton, styled } from "@mui/material";
+
 import ActivityListItem from "./ActivityListItem";
 import { useActivities } from "~/hooks/activities";
+
+export const ActivityListSkeleton = styled(Skeleton)(({ theme }) => ({
+  width: "100%",
+  height: 200,
+  borderRadius: theme.shape.borderRadius,
+}));
 
 export default function ActivityList() {
   const { activities } = useActivities();
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,39 @@
 import { type Metadata } from "next";
-import Dashboard from "./Dashboard";
+import { Box, Container, Typography } from "@mui/material";
+import { Suspense } from "react";
+import ActivityList, { ActivityListSkeleton } from "./ActivityList";
 
 export const metadata: Metadata = {
   title: "ダッシュボード - ルーティンナさん | Routinena",
 };
+
 export default function DashboardPage() {
-  return <Dashboard />;
+  return (
+    <Container
+      maxWidth="md"
+      sx={{
+        height: "100vh",
+        mt: 10,
+        padding: 0,
+        gap: 4,
+      }}
+    >
+      <Box sx={{}}>
+        <Typography
+          component="h1"
+          sx={{
+            fontWeight: 900,
+            fontSize: 30,
+          }}
+        >
+          直近の予定
+        </Typography>
+      </Box>
+      <Box sx={{ py: 2 }}>
+        <Suspense fallback={<ActivityListSkeleton />}>
+          <ActivityList />
+        </Suspense>
+      </Box>
+    </Container>
+  );
 }
