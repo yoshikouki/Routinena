6cce84736824190bd603886fc68108dd35dfca54
yoshikouki <yoshikouki@gmail.com>
2023-07-29 22:05:54 +0900

    Make the dark theme the default

--- src/app/ThemeRegistry.tsx
+++ src/app/ThemeRegistry.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import { type ReactNode } from "react";
 import {
   Experimental_CssVarsProvider as CssVarsProvider,
   experimental_extendTheme as extendTheme,
   getInitColorSchemeScript,
 } from "@mui/material/styles";
+import { type ReactNode } from "react";
 import { cssVarsThemeOptions } from "./theme";
 
 export default function ThemeRegistry({ children }: { children: ReactNode }) {
   const theme = extendTheme(cssVarsThemeOptions);
 
   return (
-    <CssVarsProvider theme={theme} defaultMode="light">
-      {getInitColorSchemeScript()}
+    <CssVarsProvider theme={theme} defaultMode="dark">
+      {getInitColorSchemeScript({
+        defaultMode: "dark",
+      })}
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       {children}
     </CssVarsProvider>
