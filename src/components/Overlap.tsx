90c90c7218061e2c118d5e3bd0d9a9f8d608ee9a
yoshikouki <yoshikouki@gmail.com>
2023-08-11 11:30:49 +0900

    Applies to MUI Transitions requirements

--- src/components/Overlap.tsx
+++ src/components/Overlap.tsx
@@ -1,50 +1,55 @@
-import { Box, Container, Slide } from "@mui/material";
-import { type ReactNode } from "react";
-import { useBottomFab } from "~/hooks/bottom-fab";
+import { Container, Slide } from "@mui/material";
+import { forwardRef, type ReactNode } from "react";
 
 interface Props {
   open: boolean;
   children: ReactNode;
 }
 
+const OverlapContainer = forwardRef<HTMLDivElement, { children: ReactNode }>(
+  ({ children, ...props }, ref) => (
+    <Container
+      ref={ref}
+      {...props}
+      sx={{
+        top: 80,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        position: "fixed",
+        zIndex: "auto",
+        overflow: "auto",
+        width: "100%",
+        height: "100vh",
+        bgcolor: "background.paper",
+        borderRadius: "28px 28px 0 0",
+        pt: 5,
+        pb: 12,
+        opacity: 0.98,
+      }}
+    >
+      {children}
+    </Container>
+  ),
+);
+OverlapContainer.displayName = "OverlapContainer";
+
 const Overlap = ({ open, children }: Props) => {
-  useBottomFab();
   if (!open) return null;
 
   return (
-    <Box>
-      <Slide
-        in={open}
-        direction={"up"}
-        mountOnEnter
-        unmountOnExit
-        timeout={{
-          enter: 200,
-          exit: 100,
-        }}
-      >
-        <Container
-          sx={{
-            top: 80,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            position: "fixed",
-            zIndex: "auto",
-            overflow: "auto",
-            width: "100%",
-            height: "100vh",
-            bgcolor: "background.paper",
-            borderRadius: "28px 28px 0 0",
-            pt: 5,
-            pb: 12,
-            opacity: 0.98,
-          }}
-        >
-          {children}
-        </Container>
-      </Slide>
-    </Box>
+    <Slide
+      in={open}
+      direction={"up"}
+      mountOnEnter
+      unmountOnExit
+      timeout={{
+        enter: 200,
+        exit: 100,
+      }}
+    >
+      <OverlapContainer>{children}</OverlapContainer>
+    </Slide>
   );
 };
 
