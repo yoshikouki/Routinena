7e9395a0b112a844dfb8ccffc16c993c762f90df
yoshikouki <yoshikouki@gmail.com>
2023-08-27 21:55:39 +0900

    Empty username and image when registering

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,8 +1,8 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import {
   getServerSession,
-  type NextAuthOptions,
   type DefaultSession,
+  type NextAuthOptions,
 } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { env } from "~/env.mjs";
@@ -43,6 +43,11 @@ export const authOptions: NextAuthOptions = {
         id: user?.id,
       },
     }),
+    signIn: ({ user }) => {
+      user.name = null;
+      user.image = null;
+      return true;
+    },
   },
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -50,15 +55,6 @@ export const authOptions: NextAuthOptions = {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 
   pages: {
