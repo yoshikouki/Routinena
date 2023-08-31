6ce7bc82d54e1fda6b21cce3b4a7e57b3dfc0c10
yoshikouki <yoshikouki@gmail.com>
2023-08-31 23:16:56 +0900

    Disable cache

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,6 @@
 import AppLayout from "~/components/layouts/AppLayout";
 import { useServerSession } from "~/hooks/server-session";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export const fetchCache = "force-no-store";
-
 export default async function DashboardLayout({
   children,
 }: {
